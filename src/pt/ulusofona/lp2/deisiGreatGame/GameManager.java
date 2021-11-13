562139c0fb91d2029be31344d14789615d1567d4
Tiago Águeda <a22001757@alunos.ulht.pt>
2021-11-13 16:53:20 +0000

    Comentários

--- src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
@@ -7,9 +7,12 @@ import java.util.*;
 public class GameManager {
     ArrayList<Programmer> programadores = new ArrayList<>();
     int nrCasas;
-    int nrTurnos = 1;
-    int turnoAtual = 0;
+    int nrTurnos = 1; // nr de turnos jogados
+    int turnoAtual = 0; // turno atual, pode ser: 0, 1, 2 ou 3
 
+    /*
+    * Função que retorna o enum
+    */
     ProgrammerColor encontrarCor(String cor){
         switch (cor){
             case "PURPLE":
@@ -33,37 +36,44 @@ public class GameManager {
     }
 
     public boolean createInitialBoard(String[][] playerInfo, int boardSize) {
+        /*
+        *  Fazer reset
+        */
         programadores.clear();
         nrTurnos = 1;
         turnoAtual = 0;
 
-        if (boardSize <= 1) {
+        if (boardSize <= 1) { // Verifica o tamanho do tabuleiro
             return false;
         }
 
         nrCasas = boardSize;
+        // Adiciona todos os jogadores à lista de jogadores
         for (int i = 0; i < playerInfo.length; i++) {
-            ArrayList<String> languages = new ArrayList();
-            languages.addAll(Arrays.asList(playerInfo[i][2].split(";")));
+            ArrayList<String> languages = new ArrayList(Arrays.asList(playerInfo[i][2].split(";")));
             Programmer player = new Programmer(playerInfo[i][1], languages, Integer.parseInt(playerInfo[i][0]), encontrarCor(playerInfo[i][3].toUpperCase()), 1, "Em Jogo");
             programadores.add(player);
         }
 
+        //Valida tamanho do tabuleiro
         if (programadores.size() > 4 || programadores.size() < 2 || nrCasas < programadores.size() * 2 ){
             return false;
         }
 
         HashSet<ProgrammerColor> colorDuplicado = new HashSet<>(); // não pode haver cores repetidas
-        HashSet<Integer> idDuplicado = new HashSet<>(); // não pode haver iDs repetidos
-        for (Programmer programador : programadores) {
-            if (programador.getId() == 0 || colorDuplicado.contains(programador.getColor()) || idDuplicado.contains(programador.getId()) ||
-                    programador.getColor() == null || programador.getName().equals("")) {
+        HashSet<Integer> idDuplicated = new HashSet<>(); // não pode haver iDs repetidos
+
+        //Percorrer todos os jogadores para fazer todas as verificações enunciadas no enunciado
+        for (Programmer programmer : programadores) {
+            if (programmer.getId() == 0 || colorDuplicado.contains(programmer.getColor()) || idDuplicated.contains(programmer.getId()) ||
+                    programmer.getColor() == null || programmer.getName().equals("")) {
                 return false;
             }
-            idDuplicado.add(programador.getId());
-            colorDuplicado.add(programador.getColor());
+            idDuplicated.add(programmer.getId());
+            colorDuplicado.add(programmer.getColor());
 
         }
+        //Ordena os jogadores por ordem dos ids
         programadores.sort(Comparator.comparing((Programmer programador1) -> programador1.getId()));
         getImagePng(1);
         getImagePng(boardSize);
@@ -78,9 +88,9 @@ public class GameManager {
             return "glory.png";
         }
 
-        for (Programmer programador: programadores) {
-            if (programador.getPosicao() == position) {
-                return "player" + programador.getColor().toString() + ".png";
+        for (Programmer programmer: programadores) {
+            if (programmer.getPosicao() == position) {
+                return "player" + programmer.getColor().toString() + ".png";
             }
         }
         return null;
@@ -91,18 +101,16 @@ public class GameManager {
     }
 
     public ArrayList<Programmer> getProgrammers(int position) {
+        // Verifica se a posição passada nos parametros está dentro do tabuleiro
         if (position < 1 || position > nrCasas){
             return null;
         }
         ArrayList<Programmer> programadoresNaPosicao = new ArrayList<>();
-        for (Programmer programador: programadores) {
-            if (programador.getPosicao() == position) {
-                programadoresNaPosicao.add(programador);
+        for (Programmer programmer: programadores) {
+            if (programmer.getPosicao() == position) { //Verifica se há jogadores na posição
+                programadoresNaPosicao.add(programmer);
             }
         }
-        /*if (programadoresNaPosicao.size() == 0 || position > nrCasas) { // Verificar numero de casas
-            return null;
-        }*/
 
         return programadoresNaPosicao;
     }
@@ -117,8 +125,8 @@ public class GameManager {
         }
         for (Programmer programador: programadores) {
             if (programador.getId() == getCurrentPlayerID()){
-                if (programador.getPosicao() + nrPositions <= nrCasas) {
-                    programador.mover(nrPositions); // falta verificar se está fora do tabuleiro
+                if (programador.getPosicao() + nrPositions <= nrCasas) { // Verifica se o jogador pode andar sem ultrapassar a meta
+                    programador.mover(nrPositions);
                 }else{
                     programador.recuar(nrPositions, nrCasas);
                 }
@@ -126,13 +134,14 @@ public class GameManager {
         }
         turnoAtual++;
         nrTurnos++;
-        if(turnoAtual >= programadores.size()) {
+        if(turnoAtual >= programadores.size()) { // Verifica se é o ultimo jogador
             turnoAtual = 0;
         }
         return true;
     }
 
     public boolean gameIsOver() {
+        //Verifica se algum jogador chegou ao fim
         for (Programmer programador: programadores){
             if (programador.getPosicao() == nrCasas){
                 return true;
@@ -164,21 +173,21 @@ public class GameManager {
     }
 
     public JPanel getAuthorsPanel() {
-        JPanel creditos = new JPanel();
-        creditos.setSize(new Dimension(300, 300));
+        JPanel credits = new JPanel();
+        credits.setSize(new Dimension(300, 300));
         JLabel linha1 = new JLabel();
         linha1.setText("Projeto Deisi Great Game");
-        creditos.add(linha1);
+        credits.add(linha1);
 
         JLabel linha2 = new JLabel();
         linha1.setText("Tiago Águeda a22001757");
-        creditos.add(linha2);
+        credits.add(linha2);
 
         JLabel linha3 = new JLabel();
         linha1.setText("João Antas a22002629");
-        creditos.add(linha3);
+        credits.add(linha3);
 
 
-        return creditos;
+        return credits;
     }
 }
