c222332cfa9f04fba7e82aab35e30ed25f341124
Tiago Águeda <a22001757@alunos.ulht.pt>
2021-11-25 23:13:15 +0000

    Verificar erros drop

--- src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
@@ -53,13 +53,17 @@ public class GameManager {
         }
         for (int i = 0; i < abyssesAndTools.length; i++){
             if (abyssesAndTools[i][0].equals("0")){
-                if (Integer.parseInt(abyssesAndTools[i][1]) < 0 || Integer.parseInt(abyssesAndTools[i][1]) > 0 || Integer.parseInt(abyssesAndTools[i][2]) < 0 || Integer.parseInt(abyssesAndTools[i][2]) > worldSize){
+                if (Integer.parseInt(abyssesAndTools[i][1]) >= 0 || Integer.parseInt(abyssesAndTools[i][1]) <= 9 || Integer.parseInt(abyssesAndTools[i][2]) > 0 || Integer.parseInt(abyssesAndTools[i][2]) <= worldSize){
                     abismos.add(criarAbismo(abyssesAndTools[i]));
+                }else{
+                    return false;
                 }
 
             }else if(abyssesAndTools[i][0].equals("1")){
-                if (Integer.parseInt(abyssesAndTools[i][1]) < 0 || Integer.parseInt(abyssesAndTools[i][1]) > 0 || Integer.parseInt(abyssesAndTools[i][2]) < 0 || Integer.parseInt(abyssesAndTools[i][2]) > worldSize) {
+                if (Integer.parseInt(abyssesAndTools[i][1]) >= 0 || Integer.parseInt(abyssesAndTools[i][1]) <= 5 || Integer.parseInt(abyssesAndTools[i][2]) > 0 || Integer.parseInt(abyssesAndTools[i][2]) <= worldSize) {
                     ferramentas.add(criarFerramentas(abyssesAndTools[i]));
+                }else{
+                    return false;
                 }
             }
         }
